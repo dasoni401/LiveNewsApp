c8c9031e40afe7e2dfb577efd3829924e6a170c4
laxman401 <laxman401@gmail.com>
2022-06-11 12:36:19 +0530

    minor change

--- src/Components/Footer/Copyright.js
+++ src/Components/Footer/Copyright.js
@@ -9,7 +9,7 @@ export default class Copyright extends Component {
                 <div className="col-sm-12">
                 <div className="d-sm-flex justify-content-between align-items-center">
                     <div className="fs-14 font-weight-600">
-                    © 2020 @ <a href="/"  className="text-white"> BootstrapDash</a>. All rights reserved by laxman.
+                    © 2022 @ <a href="/"  className="text-white"> BootstrapDash</a>. All rights reserved by laxman.
                     </div>
                     <div className="fs-14 font-weight-600">
                     Handcrafted by <a href="/"  className="text-white">BootstrapDash</a>
